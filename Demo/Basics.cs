45e9f52d2b3ea16e0814a15bc842fbb46c0931d2
Terje Sandstrom <terje@hermit.no>
2017-10-04 21:51:31 +0200

    Initial code

---
+++ Demo/Basics.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Demo
+{
+    [TestFixture]
+    public class SimpleTest
+    {
+        [Test]
+        public void JustChecking()
+        {
+            Assert.That(true);
+        }
+
+        [TestCase(42, 1)]
+        public void JustCheckingParameters(int number, int limit)
+        {
+            Assert.That(number,Is.GreaterThan(limit));
+        }
+    }
+}
